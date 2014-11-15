ac597767fa3f2870e591423734a690bc09704ae7
imoseyon <imoseyon@gmail.com>
2014-11-14 17:04:39 -0800

    mmc: core: Expose use_spi_crc module parameter in sysfs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -90,7 +90,7 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  */
 
 bool use_spi_crc = 0;
-module_param(use_spi_crc, bool, 0);
+module_param(use_spi_crc, bool, 0644);
 
 /*
  * We normally treat cards as removed during suspend if they are not
